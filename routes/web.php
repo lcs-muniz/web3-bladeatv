28efd2c8589cfa2799406a8bdd6cbd909e392c5d
lcs-muniz <lucas.muniz.ifpr@gmail.com>
2024-10-03 01:39:04 -0300

    feat: initial commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-page', function () {
+    return 'Bem-vindo à minha página!';
+});
